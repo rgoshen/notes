7c756191bbba435c1a852d2ea367dc113f3d7323
Richard Goshen <rick.goshen@gmail.com>
2022-06-21 22:04:12 -0700

    add project mind map

---
+++ knowledge_base.mm
@@ -0,0 +1,140 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="knowledge_base" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1655587216943"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="languages" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1472301387" CREATED="1655587132806" MODIFIED="1655587225610">
+<edge COLOR="#00ff00"/>
+<node TEXT="frontend" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1935093374" CREATED="1655587412884" MODIFIED="1655587428590">
+<node TEXT="html" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1140376965" CREATED="1655587966361" MODIFIED="1655587981028"/>
+<node TEXT="css" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1069596092" CREATED="1655587985606" MODIFIED="1655588032796"/>
+<node TEXT="frameworks/libraries" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_802509275" CREATED="1655588088286" MODIFIED="1655588103109">
+<node TEXT="Reactjs" LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_1879186444" CREATED="1655588140018" MODIFIED="1655588187890"/>
+<node TEXT="JWT" LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_1793809552" CREATED="1655588284587" MODIFIED="1655588319769"/>
+</node>
+<node TEXT="JavaScript" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_384993560" CREATED="1655588217702" MODIFIED="1655588237570"/>
+</node>
+<node TEXT="backend" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1471546786" CREATED="1655587442969" MODIFIED="1655587454089">
+<node TEXT="Python" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1637452881" CREATED="1655588054342" MODIFIED="1655588064178"/>
+<node TEXT="frameworks/libraries" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_504711696" CREATED="1655588114926" MODIFIED="1655588130176">
+<node TEXT="Nodejs" LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_1258220295" CREATED="1655588171017" MODIFIED="1655588326601"/>
+</node>
+<node TEXT="Java" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1446419132" CREATED="1655588305979" MODIFIED="1655588312953"/>
+<node TEXT="C" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_194386492" CREATED="1655588751235" MODIFIED="1655588788764"/>
+<node TEXT="C++" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_58734496" CREATED="1655588795322" MODIFIED="1655588806230"/>
+<node TEXT="Go" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1683307147" CREATED="1655588823872" MODIFIED="1655588831561"/>
+</node>
+<node TEXT="Scripting" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_197902678" CREATED="1655589910315" MODIFIED="1655589919616">
+<node TEXT="bash" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1560528382" CREATED="1655589926098" MODIFIED="1655589935751"/>
+</node>
+</node>
+<node TEXT="devops" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_485076157" CREATED="1655587163261" MODIFIED="1655587206928">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Git/GitHub" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_683410973" CREATED="1655588905951" MODIFIED="1655588988397"/>
+<node TEXT="Terraform" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1641629394" CREATED="1655588924521" MODIFIED="1655588988400"/>
+<node TEXT="Circle CI" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1570951374" CREATED="1655588943735" MODIFIED="1655588988401"/>
+<node TEXT="AWS Lambdas" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_499601827" CREATED="1655832884447" MODIFIED="1655832904903"/>
+</node>
+<node TEXT="cheatsheets" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_43585023" CREATED="1655587242984" MODIFIED="1655587259776">
+<edge COLOR="#00ffff"/>
+<node TEXT="keyboard short cuts" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_298948156" CREATED="1655588463930" MODIFIED="1655588483201">
+<node TEXT="VS Code" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_74664379" CREATED="1655588604728" MODIFIED="1655588624398"/>
+<node TEXT="jetbrains" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1803172766" CREATED="1655588633310" MODIFIED="1655588660714"/>
+</node>
+</node>
+<node TEXT="databases" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1819537155" CREATED="1655587356529" MODIFIED="1655587369454">
+<edge COLOR="#7c0000"/>
+<node TEXT="sql" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_892561983" CREATED="1655587533566" MODIFIED="1655587547188">
+<node TEXT="PostgreSQL" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1786569265" CREATED="1655587639747" MODIFIED="1655587659322"/>
+</node>
+<node TEXT="no sql" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_523923130" CREATED="1655587554730" MODIFIED="1655587570821">
+<node TEXT="MongoDB" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1409126589" CREATED="1655587671877" MODIFIED="1655587687802"/>
+<node TEXT="AWS DynamoDB" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_568510028" CREATED="1655587711927" MODIFIED="1655832875232"/>
+</node>
+</node>
+<node TEXT="computer science" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1694830741" CREATED="1655587581729" MODIFIED="1655588375571">
+<edge COLOR="#00007c"/>
+<node TEXT="web dev fundamentals" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1140547321" CREATED="1655587800079" MODIFIED="1655587860226"/>
+<node TEXT="problem solving" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_328574732" CREATED="1655587827431" MODIFIED="1655587864817"/>
+<node TEXT="DSA" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1852829171" CREATED="1655587888127" MODIFIED="1655587897080"/>
+<node TEXT="Software Architecture" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_865136324" CREATED="1655588854870" MODIFIED="1655588877163"/>
+</node>
+<node TEXT="misc" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_818510512" CREATED="1655587914112" MODIFIED="1655587923779">
+<edge COLOR="#007c00"/>
+<node TEXT="project management" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_456449518" CREATED="1655587929178" MODIFIED="1655587943750"/>
+</node>
+<node TEXT="operating systems" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_557355017" CREATED="1655588397348" MODIFIED="1655588412554">
+<edge COLOR="#7c007c"/>
+<node TEXT="windows" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_526353502" CREATED="1655588538476" MODIFIED="1655588545982"/>
+<node TEXT="linux" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_627902385" CREATED="1655588556013" MODIFIED="1655588569167"/>
+<node TEXT="mac os" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1442087075" CREATED="1655588573759" MODIFIED="1655588586452"/>
+</node>
+<node TEXT="Tutorials" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_907087852" CREATED="1655588518811" MODIFIED="1655588531568">
+<edge COLOR="#007c7c"/>
+</node>
+</node>
+</map>
